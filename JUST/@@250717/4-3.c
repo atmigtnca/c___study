f3984bda3fd9ced70bac2fdb15839a8a25e6a6ff
atmigtnca <goodmorningyb@gmail.com>
2025-07-10 20:03:44 +0900

    250710

---
+++ JUST/@@250717/4-3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void){
+    int input;
+
+    printf("숫자를 하나 입력하세요.>");
+    scanf("%d",&input);
+
+    if(input==0){
+        printf("입력하신 숫자는 0입니다.\n");
+    } else {
+        printf("입력하신 숫자는 0이 아닙니다.\n");
+    }
+
+    return 0;
+}
