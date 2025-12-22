313ca2c45324a94a7a240ebc2df73be1716462d0
atmigtnca <whdgh9173@jbnu.ac.kr>
2025-12-22 18:18:43 +0900

    251222

---
+++ BAEKJOON/12/251222/14713.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+vector<queue<string>> prt;
+int N;
+
+void input()
+{
+    cin >> N;
+    prt.resize(N);
+
+    string tmp;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    return 0;
+}
