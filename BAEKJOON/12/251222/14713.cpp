6841edeb895c9dedcb2e1b85f64a273e9ea58ab7
atmigtnca <whdgh9173@jbnu.ac.kr>
2025-12-23 02:23:57 +0900

    251223

--- BAEKJOON/12/251222/14713.cpp
+++ BAEKJOON/12/251222/14713.cpp
@@ -7,18 +7,82 @@ using ll = long long;
 vector<queue<string>> prt;
 int N;
 
+string check()
+{
+    string cseteram, word;
+    getline(cin, cseteram);
+
+    for (int c = 0; c <= cseteram.size(); c++)
+    {
+        if (cseteram[c] == ' ' || c == cseteram.size())
+        {
+            bool fnd = true;
+            for (int i = 0; i < N; i++)
+            {
+                if (prt[i].empty())
+                {
+                    continue;
+                }
+                string cmp = prt[i].front();
+                if (cmp == word)
+                {
+                    fnd = false;
+                    prt[i].pop();
+                    break;
+                }
+            }
+            if (fnd)
+            {
+                return "Impossible";
+            }
+            word = "";
+            continue;
+        }
+        word += cseteram[c];
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        if (!prt[i].empty())
+        {
+            return "Impossible";
+        }
+    }
+
+    return "Possible";
+}
+
 void input()
 {
     cin >> N;
+    cin.ignore();
     prt.resize(N);
 
     string tmp;
+    for (int s = 0; s < N; s++)
+    {
+        getline(cin, tmp);
+        string word = "";
+        for (int c = 0; c <= tmp.size(); c++)
+        {
+            if (tmp[c] == ' ' || c == tmp.size())
+            {
+                prt[s].push(word);
+                word = "";
+                continue;
+            }
+            word += tmp[c];
+        }
+    }
 }
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
+
+    input();
+    cout << check() << '\n';
 
     return 0;
 }
